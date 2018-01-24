4b8d7fe071e5b5a2621f566132c8d5824fe8d37e
Shaun <soo.shaun@gmail.com>
2018-01-24 16:58:26 +1100

    git commit -m 'Update landing Page'

--- web/src/Components/Landing.js
+++ web/src/Components/Landing.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React from 'react'
+import { showcase, button } from '../style/LandingPage.css'
+import { Link } from 'react-router-dom'
+import { Layout } from 'antd';
+const { Content } = Layout;
 
-const LandingPage = () => (
-  <div>
-    <h1>Landing Page</h1>
-  </div>
-);
 
-export default LandingPage;
+const landingPage = () => {
+  return (
+    <Content>
+      <div className="showcase">
+        <h1>Welcome To Xiao Ping Gao</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi officiis ipsum officia numquam expedita ullam.</p>
+        <Link to="/signin" className="button">SIGN IN</Link>
+      </div>
+    </Content>
+
+  )
+
+}
+
+export default landingPage
